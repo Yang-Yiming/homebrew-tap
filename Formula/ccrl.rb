b2f424e44460665919fa43bff303fd6e444b6cbd
Yang-Yiming <44901560@qq.com>
2026-02-27 17:32:08 +0800

    fix rb

--- Formula/ccrl.rb
+++ Formula/ccrl.rb
@@ -5,7 +5,13 @@ class Ccrl < Formula
   sha256 "b665b6461b195661babd4ab3da4291d9a309b28982d4a170be6946332fbf0ec3"
   license "MIT"
 
+  depends_on "rust" => :build
+
   def install
     system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "ccrl", shell_output("#{bin}/ccrl --help")
   end
 end
