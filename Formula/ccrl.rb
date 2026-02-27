1a875d26226e2e7b8cc7c3e72597681dea0302ae
Yang-Yiming <44901560@qq.com>
2026-02-27 17:22:15 +0800

    upload ccrl

---
+++ Formula/ccrl.rb
@@ -0,0 +1,11 @@
+class ccrl < Formula
+  desc "A Lightweight ClaudeCode Router"
+  homepage "https://github.com/Yang-Yiming/cc-router-lite"
+  url "https://github.com/Yang-Yiming/cc-router-lite/archive/refs/tags/v0.1.tar.gz"
+  sha256 "b665b6461b195661babd4ab3da4291d9a309b28982d4a170be6946332fbf0ec3"
+  license "MIT"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
