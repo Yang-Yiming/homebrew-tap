35fd4eddc773805d6bd36585fc2c8dc323cd3f94
Yang-Yiming <44901560@qq.com>
2026-02-27 17:39:19 +0800

    fix rb

--- Formula/ccrl.rb
+++ Formula/ccrl.rb
@@ -1,14 +1,16 @@
 class Ccrl < Formula
   desc "A Lightweight ClaudeCode Router"
   homepage "https://github.com/Yang-Yiming/cc-router-lite"
-  url "https://github.com/Yang-Yiming/cc-router-lite/archive/refs/tags/v0.1.tar.gz"
-  sha256 "b665b6461b195661babd4ab3da4291d9a309b28982d4a170be6946332fbf0ec3"
+  version "0.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/Yang-Yiming/cc-router-lite/archive/refs/tags/v#{version}.tar.gz"
+      sha256 "b665b6461b195661babd4ab3da4291d9a309b28982d4a170be6946332fbf0ec3"
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "ccrl"
   end
 
   test do
