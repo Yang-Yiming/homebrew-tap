554aa58d76689c6d0b8eae98d276f14dddf6966c
WaggingHorse <57488194+Yang-Yiming@users.noreply.github.com>
2026-03-05 21:32:06 +0800

    update flicker to v0.1.1

--- Formula/flicker.rb
+++ Formula/flicker.rb
@@ -1,13 +1,13 @@
 class Flicker < Formula
   desc "Lightweight CLI notebook"
   homepage "https://github.com/Yang-Yiming/Flicker"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Yang-Yiming/Flicker/releases/download/v#{version}/flicker-aarch64-apple-darwin.tar.gz"
-      sha256 "8a336b69f3485304d3f68f8e76181014a0bf4b9fde08a13f3b2c98f16bed8153"
+      sha256 "de2654312d1b0888402e9b40b28967c200ab597c3bb282e994f9af587fcfbd90"
     end
   end
 
