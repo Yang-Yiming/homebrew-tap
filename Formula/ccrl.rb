41529057ce13fd47fdc64411f6456a6861111102
WaggingHorse <57488194+Yang-Yiming@users.noreply.github.com>
2026-03-06 16:38:58 +0800

    update ccrl to v0.3.0

--- Formula/ccrl.rb
+++ Formula/ccrl.rb
@@ -1,13 +1,13 @@
 class Ccrl < Formula
   desc "A Lightweight ClaudeCode Router"
   homepage "https://github.com/Yang-Yiming/cc-router-lite"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Yang-Yiming/cc-router-lite/releases/download/v#{version}/ccrl-aarch64-apple-darwin.tar.gz"
-      sha256 "1a99fdac4b5497686c8daf21916358a7f08c57db2536347dc6939c7e6f2298ab"
+      sha256 "48d3af6c2c63307599a0b2662412e20efef877efc3431a1c153c11acf7d81da1"
     end
   end
 
